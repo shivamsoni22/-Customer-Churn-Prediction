570b2cc2d9add35fc88e2e7c1f5d2588bb28bb07
shivamsoni22 <136327660+shivamsoni22@users.noreply.github.com>
2024-10-31 12:34:06 +0530

    update code

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-from src.data_preprocessing import load_data, clean_data
+from src.data_preprocessing import load_data
 from src.model_training import train_model
 from src.model_evaluation import evaluate_model
 
 def main():
-    # Load and clean data
-    data = load_data('D:\-Customer-Churn-Prediction\Data\customer_churn_data.csv')
-    clean_data_df = clean_data(data)
+    # Load the dataset
+    data = load_data('D:\\-Customer-Churn-Prediction\\Data\\customer_churn_data.csv')  # Updated file path
 
     # Train the model
-    model, X_test, y_test = train_model(clean_data_df)
+    model, X_test, y_test = train_model(data)  # Get the model and test data
 
     # Evaluate the model
     evaluate_model(model, X_test, y_test)
 
 if __name__ == "__main__":
     main()
-
